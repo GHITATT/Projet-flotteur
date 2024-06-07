d7375362a41174ec32394eaad5fec541aa4f897a
Wazzap101 <monjauzeajb@gmail.com>
2024-06-07 16:45:29 +0200

    Add files via upload

--- projet flotteur Monjauze.py
+++ projet flotteur Monjauze.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import interpolate
+import math
 
 fichier = open("liste_rho.txt")
 lignes = fichier.readlines()
@@ -43,19 +44,26 @@ def f1(depth):
     return f(depth)
 
 
-mf = 40 #en kg
+mf = 60 #en kg
 V = mf/f(10) #en m3
 D = 11.3e-2 #en m
 S = np.pi*(D/2)**2 #en m3
 C = 1
-N = 30000
-h = 0.05
+N = 6000
 n = 0.00108
-t = np.linspace(0, N, N+1)
-g = 9.8 #m/s-2
-dt = 100/N
-Vmin = mf/1032.09
-Vmax = mf/1025.22
+t, dt = np.linspace(0, N, N+1, retstep=True)
+h=dt
+g1 = 9.81 #m/s-2
+
+nvm = 0.9
+pas = 5e-3  #en m
+diam= 25e-3 #en m
+tani = pas/(np.pi*diam)
+phi = math.atan(tani/nvm)-math.atan(tani)
+nem = math.tan(math.atan(tani)-phi)/tani
+nmoteur = 1
+nreduc = 1
+
 
 
 def g(X, V):
@@ -86,12 +94,31 @@ def euler():
     plt.legend()
     plt.show()
 
-#euler()
+
+
+Patm =1.01325e5 #Pa
+K = 2.2e9 #Pa
+
+def Pression(z):
+    return  z/10
+
+def f2(z):
+    P = Pression(z)
+    return 1025.22*(1+(P-Patm)/K)
+dVmax = (np.pi*(42.5e-3)**2) *0.3/120
+
 
 def correc(kp, ki, kd, z_cible_tab, N_tab):
-    V = mf/f1(0)
+    V = mf/1025.22
+    V1 = np.zeros(N+1)
+    E = np.zeros(N + 1)
+    V1[0] = V
+    z_point = np.zeros(N+1)
+    z_2point = np.zeros(N+1)
+    dVmax = 2.2578e-6*dt
+    dV1 = np.zeros(N+1)
     for i in range(N):
-        z_cible = trajectoire(i,z_cible_tab, N_tab)
+        z_cible = trajectoire(i, z_cible_tab, N_tab)
         k1 = h*g(X[i], V)
         k2 = h*g(X[i] + k1/2, V)
         k3 = h*g(X[i]+ k2/2, V)
@@ -99,10 +126,21 @@ def correc(kp, ki, kd, z_cible_tab, N_tab):
         X[i+1] = X[i] + (1/6)*(k1 + 2*k2 + 2*k3 + k4)
         y[i+1] = X[i+1][0]
         v[i+1] = X[i+1][1]
-        V = mf / f1(y[i + 1])
-        V_prec = mf / f1(y[i])
-        dV = kp * (mf/f1(z_cible)-V) + ki*(mf/f1(z_cible)-V)*dt + kd * ((mf/f1(z_cible)-V) - (mf/f1(z_cible)-V_prec))/dt
-        V += dV
+        z_point[i+1] = (y[i+1]-y[i])/dt
+        z_2point[i+1] = (z_point[i+1]-z_point[i])/dt
+        if True or not i%5:
+            dV = (kp * z_point[i+1] - ki*(z_cible - y[i + 1]) + kd * z_2point[i+1])
+        else:
+            dV=0
+        dV = min(max(dV, -dVmax), dVmax)
+        dV1[i+1] = dV
+        V += dV*dt
+        V1[i+1] = V
+        if dV >= 0:
+            E[i + 1] = E[i] + 1/(nvm * nreduc * nmoteur) * dt * -dV * Pression(y[i]) * 100000
+        else:
+            E[i + 1] = E[i] + (nem * nreduc * nmoteur) * dt * -dV * Pression(y[i]) * 100000
+    print(E[-1] - E[0])
     plt.plot(t, -np.array(y), '--', label='Profondeur')
     plt.plot(t, v, label='vitesse')
     plt.xlabel("temps")
@@ -111,71 +149,38 @@ def correc(kp, ki, kd, z_cible_tab, N_tab):
     plt.legend()
     plt.show()
 
-def P2(z_cible):
-    P = (z_cible / 10 + 1)
-    return P
-
-
-def correc2(kp, ki, kd, z_cible_tab, N_tab, Vmin, Vmax):
-    V = mf/f1(0)
-    V_tab = [V]
-    for i in range(N):
-        z_cible = trajectoire(i,z_cible_tab, N_tab)
-        k1 = h*g(X[i], V)
-        k2 = h*g(X[i] + k1/2, V)
-        k3 = h*g(X[i] + k2/2, V)
-        k4 = h*g(X[i]+k3, V)
-        X[i+1] = X[i] + (1/6)*(k1 + 2*k2 + 2*k3 + k4)
-        y[i+1] = X[i+1][0]
-        v[i+1] = X[i+1][1]
-        P = P2(y[i+1])
-        P_prec = P2(y[i])
-        dV = kp * (P-P2(z_cible)) + ki*(P-P2(z_cible))*dt+ kd * ((P-P2(z_cible)) - (P_prec-P2(z_cible)))/dt
-        dV = regulation_dV(dV)
-        V += dV
-        V = regulation_V(V, Vmin, Vmax)
-        V_tab.append(V)
-    plt.plot(t, -np.array(y), '--', label='Profondeur')
-    plt.plot(t, v, label='vitesse')
+    plt.plot(t, V1, label='Volume')
     plt.xlabel("temps")
-    plt.title("z = f(t)"+', kp = '+str(kp)+', ki = '+str(ki)+', kd = '+str(kd))
+    plt.title("V = f(t)")
     plt.grid()
     plt.legend()
     plt.show()
 
-    # plt.plot(t, V_tab, label='Volume')
-    # plt.xlabel("temps")
-    # plt.title("Volume du flotteur")
-    # plt.grid()
-    # plt.legend()
-    # plt.show()
+    plt.plot(t, dV1, label='dVolume')
+    plt.xlabel("temps")
+    plt.title("dV = f(t)")
+    plt.grid()
+    plt.legend()
+    plt.show()
+
+    plt.plot(t, E, label='Energie')
+    plt.xlabel("temps")
+    plt.title("E = f(t)")
+    plt.grid()
+    plt.legend()
+    plt.show()
+
 
 def trajectoire(i, z_cible_tab, N_tab):
-    if i <= N_tab[0]:
-        z_cible = z_cible_tab[0]
-    elif i > N_tab[1]:
-        z_cible = z_cible_tab[2]
-    else:
-        z_cible = z_cible_tab[1]
-    # z_cible  = z_cible_tab[0]
+    # if i <= N_tab[0]:
+    #     z_cible = z_cible_tab[0]
+    # elif i > N_tab[1]:
+    #     z_cible = z_cible_tab[2]
+    # else:
+    #     z_cible = z_cible_tab[1]
+    z_cible = z_cible_tab[0]
     return z_cible
 
-def regulation_V(V, Vmin, Vmax):
-    if V < Vmin:
-        V = Vmin
-    if V > Vmax:
-        V = Vmax
-    return V
 
-def regulation_dV(dV):
-    facteur = 2/7
-    limite = 1.25*42.5**2*3.14*0.000000001*facteur
-    if abs(dV) > limite:
-        if dV < 0 :
-            dV = -limite
-        else :
-            dV = limite
-    return dV
+correc(1e-4, 1e-7, 1e-2, [100,300,0], [5/11*N,6/11*N])
 
-
-correc2(50, 10, 120, [10,25,2], [5*N/11,7*N/11], Vmin, Vmax)
